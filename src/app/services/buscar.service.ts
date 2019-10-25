b41119b792e1a9c958881cf7e1063ed41c29ea82
Daniel33333 <dbynm14@hotmail.com>
2019-10-25 16:20:54 -0600

    first commit

---
+++ src/app/services/buscar.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuscarService {
+
+  server = 'http://api.slothsoftware.com/';
+
+  constructor(private http: HttpClient) { }
+
+  postData(body) {
+    const file  = 'api-server.php';
+    const options= {
+      headers: {
+        'Content - Type': 'application/x-www-form-urlencoded'
+      }
+    };
+    //console.log(body);}
+    return new Promise(resolve =>{
+      this.http
+      .post(this.server + file, JSON.stringify(body), options)
+      .subscribe(data => {
+        resolve(data);
+      });
+    });
+  }
+}
